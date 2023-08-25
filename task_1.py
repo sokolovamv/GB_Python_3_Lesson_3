904627ab9758069e9b5b7272ef20261815fb51f2
Maria <masya.2506@mail.ru>
2023-08-25 11:38:06 +0300

    task_1_v2

--- task_1.py
+++ task_1.py
@@ -2,10 +2,16 @@
 # Вернуть список с дублирующимися элементами. 
 # В результирующем списке не должно быть дубликатов.
 
+# список повторяющихся элементов
 user_list = [1, 2, 3, 2, 3, 3, 'xxx', 'a', 'aa', 'aa']
 new_list = []
 
+# список с дублирующимися элементами + проверка, что символ дублируется
 for i in user_list:
     if user_list.count(i) > 1 and i not in new_list:
         new_list.append(i)
 print(new_list)
+
+# второй вариант с множествами
+new_list_2 = [i for i in set(user_list) if user_list.count(i) > 1]
+print(new_list_2)
