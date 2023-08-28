499cfe029301c962598edeb10530ad32412e0c92
Maria <masya.2506@mail.ru>
2023-08-28 18:01:22 +0300

    without_task_6

--- task_8_for_seminar.py
+++ task_8_for_seminar.py
@@ -14,12 +14,6 @@ def filling_of_backpack(amount_of_people: int) -> dict:
         dict_of_clothes[name] = tuple(input('Введите через пробел вещи, которые он взял').split())
     return dict_of_clothes
 
-def unique_clothes(in_dict: dict) -> list:
-    list_of_dict = [value for value in in_dict.values()]
-    compare_clothes = list_of_dict[0]
-    for i in range(len(list_of_dict) - 1):
-        compare_clothes = set(compare_clothes) & set(list_of_dict[i+1])
-    return compare_clothes
 
 
 amount_of_friends  = int(input('Введите количество друзей, которые собираются в поход: '))
